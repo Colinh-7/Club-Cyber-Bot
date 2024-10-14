130f52ec03836aa9f3a5897ee00f0477a46cf76f
Colin <colinh@live.be>
2024-10-14 20:50:51 +0200

    Bot de test

--- bot.py
+++ bot.py
@@ -7,5 +7,9 @@ intents.message_content = True
 
 bot = commands.Bot(command_prefix="!", intents = intents)
 
+@bot.command()
+async def bonjour(ctx):
+    await ctx.send(f"Bonjour {ctx.author}")
+
 token = os.environ['TOKEN_BOT_DISCORD']
 bot.run(token)
